032acd633482425396a55cfff4ff31fb82187642
Chantal Munja Buerger <c_buerger@gmx.net>
2023-11-17 11:00:17 +0100

    added stack_len

--- nodes.c
+++ nodes.c
@@ -6,11 +6,31 @@
 /*   By: chbuerge <chbuerge@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/08 12:55:03 by chbuerge          #+#    #+#             */
-/*   Updated: 2023/11/10 10:18:59 by chbuerge         ###   ########.fr       */
+/*   Updated: 2023/11/17 10:58:51 by chbuerge         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
+
+// calculate and return the length of my stack
+// stack is a pointer to the first node of the linked list
+int	stack_len(t_stack *stack)
+{
+	int	i;
+
+	i = 0;
+	if (!stack)
+		return (0);
+	// loop until the end of the stack (until stack == NULL)
+	while (stack)
+	{
+		// traversing through the elements, moving to the next node in the list
+		stack = stack->next;
+		i++;
+	}
+	return (i);
+}
+
 
 // creating a new node
 t_stack *create_node(int value)
